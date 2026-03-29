f113972e85d77063ba97330b68e5e97e592f1902
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-03-29 18:46:12 +0000

    feat : match function with pattern an text as param and search function with files and pattern parameters

--- apps/core/src/fileSearcher.ts
+++ apps/core/src/fileSearcher.ts
@@ -10,4 +10,26 @@ export class FileSearcher {
             maxResults: options?.maxResults ?? 10,
         };
     }
+
+    private isMatch(pattern: string, text: string): boolean {
+        let i = 0;
+        let j = 0;
+      
+        while (i < pattern.length && j < text.length) {
+          if (pattern[i].toLowerCase() === text[j].toLowerCase()) {
+            i++;
+          }
+          j++;
+        }
+      
+        return i === pattern.length;
+      }
+
+    search(files : string[], pattern : string) {
+        return files.filter(file => this.isMatch(pattern,file))
+        .map((file) => ({
+            name: file,
+            score :1
+        }))
+        }
 }
