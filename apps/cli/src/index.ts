0c8436808e9745b8a587c46977f3c5ed38ee16af
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-04-01 22:29:13 +0000

    feat : using select method from inquirer listing all search matches and navigating between them using the arrow keys and selecting one with the enter key

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,19 +2,32 @@
 import { Command } from "commander";
 import { FileSearcher } from "@zap/core";
 import * as fs from "fs";
+import { select } from "@inquirer/prompts";
 
 const program = new Command();
 
 program
   .name("zap")
   .argument("<pattern>")
-  .action((pattern) => {
+  .action(async (pattern) => {
     const searcher = new FileSearcher();
-
     const files = fs.readdirSync(process.cwd());
     const results = searcher.search(files, pattern);
 
-    console.log(results);
+    if (results.length === 0) {
+      console.log("No match");
+      return;
+    }
+
+    const selected = await select({
+      message: "Select:",
+      choices: results.map(r => ({
+        name: r.name,
+        value: r.name
+      }))
+    });
+    
+    console.log(selected);
   });
 
 program.parse();
