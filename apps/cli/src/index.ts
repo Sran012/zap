1d40f49ed51f8066b4214ca09e90d307dcb402e6
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-04-04 07:09:44 +0000

    feat(temp) : simulating change dir command

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -9,13 +9,19 @@ const program = new Command();
 program
   .name("zap")
   .argument("<pattern>")
-  .action(async (pattern) => {
+  .option('--plain')
+  .action(async (pattern:string,options:any) => {
     const searcher = new FileSearcher();
     const files = fs.readdirSync(process.cwd());
     const results = searcher.search(files, pattern);
 
     if (results.length === 0) {
       console.log("No match");
+      return;
+    }
+
+    if(options.plain){
+      process.stdout.write(results[0].name);
       return;
     }
 
@@ -27,7 +33,7 @@ program
       }))
     });
     
-    console.log(selected);
+    process.stdout.write(selected);
   });
 
 program.parse();
